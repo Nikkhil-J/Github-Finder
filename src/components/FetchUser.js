0143b3455119b7e4a087891282cd21915fcb3087
Nikkhil-J <nikhil.bits02@gmail.com>
2020-05-10 04:04:42 +0530

    avatar added

--- src/components/FetchUser.js
+++ src/components/FetchUser.js
@@ -1,12 +1,20 @@
 import React from 'react'
+import avatar from './avatar.jpg'
 
 function FetchUser(props) {
-    console.log(props.data)
+
     return (
         <div style={divStyle} className="card">
+                <div>
+                    {
+                        props.data.avatar_url != null ? <img src={props.data.avatar_url}></img>  : <img src={avatar}></img> 
+                    }
+                </div>
                 <div className="flexclass" style={{fontWeight:'400'}}><span>Login Name:</span> <span className="spancard" style={spanStyle}>{props.data.login}</span></div>
                 <div className="flexclass" style={{fontWeight:'400'}}>User Id: <span className="spancard" style={spanStyle}>{props.data.id}</span></div>
                 <div className="flexclass" style={{fontWeight:'400'}}>Public Repos: <span className="spancard" style={spanStyle}>{props.data.public_repos}</span> </div>
+                <div className="flexclass" style={{fontWeight:'400'}}>Followers: <span className="spancard" style={spanStyle}>{props.data.followers}</span> </div>
+
         </div>
     )
 }
