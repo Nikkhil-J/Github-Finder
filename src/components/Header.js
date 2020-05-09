898cf5858667be134fd71ccace194510d5fac9ec
Nikkhil-J <nikhil.bits02@gmail.com>
2020-05-10 04:19:23 +0530

    icon added using react-icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import {BsPieChartFill} from 'react-icons/bs'
+
 function Header() {
     return (
         <div>
             <header  className="headerclass">
-            Github User Finder
+            <BsPieChartFill size="1.8rem"/> Github User Finder
             <div style={{textDecoration:'underline' , cursor: 'pointer'}}>
                 <Link className="linkClass"  to="/home">Home</Link>|
                 <Link className="linkClass" to="/form">Users</Link>|
